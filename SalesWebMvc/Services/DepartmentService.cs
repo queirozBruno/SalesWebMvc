214799598b6b86d857843c0740a348e90f4113b5
queirozBruno <95.brunoqueiroz@gmail.com>
2021-09-27 23:03:26 -0300

    SellerFormViewModel e componente select para departamento

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context; //Previne que a dependência seja alterada
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -43,6 +43,7 @@ namespace SalesWebMvc
             //options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
             services.AddScoped<SeedingService>(); //Registra o nosso serviço no sistema de injeção de dependência da aplicação
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
